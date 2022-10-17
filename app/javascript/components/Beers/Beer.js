af76cc86785ea3a1b0234d01d1a9bc247f54b485
Oleh Saienko <saenko.oleg1993@gmail.com>
2022-10-17 21:36:57 +0300

    add styled components + style Beers

--- app/javascript/components/Beers/Beer.js
+++ app/javascript/components/Beers/Beer.js
@@ -1,18 +1,58 @@
 import React from "react"
 import { BrowserRouter as Router, Link } from 'react-router-dom'
+import styled from "styled-components"
+
+const Card = styled.div`
+    border: 1px solid #efefef;
+    background: #fff;
+    text-align: center;
+`
+
+const BeerLogo = styled.div`
+    width: 50px;
+    text-align: center;
+    margin-left: auto;
+    margin-right: auto;
+    padding-top: 10px;
+    
+    img {
+        height: 50px;
+        width: 50px;
+        border-radius: 100%;
+        border: 1px solid #efefef;
+    }
+`
+
+const BeerName = styled.div`
+    padding: 20px 0 10px 0;
+`
+const LinkWrapper = styled.div`
+    margin: 30px 0 20px 0;
+    height: 50px;
+    
+    a {
+        color: #fff;
+        background: #000;
+        border-radius: 4px;
+        padding: 10px 50px;
+        border: 1px solid #000;
+        width: 100%;
+        text-decoration: none;
+    }
+`
 
 const Beer = (props) => {
     return (
-        <div className="card">
-            <div className="beer-logo">
+        <Card>
+            <BeerLogo>
                 <img src={props.attributes.image_url} alt={props.attributes.name}/>
-            </div>
-            <div className="beer-name">{props.attributes.name}</div>
+            </BeerLogo>
+            <BeerName>{props.attributes.name}</BeerName>
             <div className="beer-score">{props.attributes.avg_score}</div>
-            <div className="beer-link">
-                <Link to={`/beers/${props.attributes.slug}`}>Check out this Beer</Link>
-            </div>
-        </div>
+            <LinkWrapper>
+                <Link to={`/beers/${props.attributes.slug}`}>View Beer</Link>
+            </LinkWrapper>
+        </Card>
     )
 }
 
--- app/javascript/components/Beers/Beers.js
+++ app/javascript/components/Beers/Beers.js
@@ -1,7 +1,35 @@
 import React, { useState, useEffect } from "react"
 import axios from "axios"
+import styled from "styled-components"
 
 import Beer from "./Beer"
+
+const Home = styled.div`
+    text-align: center;
+    max-width: 1200px;
+    margin-left: auto;
+    margin-right: auto;
+`
+
+const Header = styled.div`
+    padding: 100px 100px 10px 100px;
+    h1 {
+        font-size: 42px;
+    }
+`
+
+const Subheader = styled.div`
+    font-weight: 300;
+    font-size: 26px;
+`
+
+const Grid = styled.div`
+    display: grid;
+    grid-template-columns: repeat(4, 1fr);
+    grid-gap: 20px;
+    width: 100%;
+    padding: 20px;
+`
 
 const Beers = () => {
     const [beers, setBeers] = useState([])
@@ -9,9 +37,7 @@ const Beers = () => {
     useEffect(() => {
         // get from api and update state
         axios.get('/api/v1/beers.json')
-            .then(resp => {
-                setBeers(resp.data.data)
-            })
+            .then(resp => setBeers(resp.data.data))
             .catch(resp => console.log(resp))
     }, [beers.length])
 
@@ -25,15 +51,15 @@ const Beers = () => {
     })
 
     return (
-        <div className="home">
-            <div className="header">
+        <Home>
+            <Header>
                 <h1>IBDB - Internet Beer Database</h1>
-                <div className="subheader">Honest, unbiased Beer reviews.</div>
-            </div>
-            <div className="grid">
+                <Subheader>Honest, unbiased Beer reviews.</Subheader>
+            </Header>
+            <Grid>
                 {grid}
-            </div>
-        </div>
+            </Grid>
+        </Home>
     )
 }
 
