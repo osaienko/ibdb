6a60842daaec0cf51f479bffed2eed9677296609
Oleh Saienko <saenko.oleg1993@gmail.com>
2022-10-18 12:01:36 +0300

    fill in the beer component

--- app/javascript/components/Beer/Beer.js
+++ app/javascript/components/Beer/Beer.js
@@ -1,7 +1,41 @@
-import React from "react"
+import React, { useState, useEffect } from 'react'
+import {useParams} from 'react-router-dom'
+import axios from 'axios'
 
-const Beer = () => {
-    return (<div>This is the Beers#show view for our app.</div>)
+import Header from './Header'
+
+const Beer = (props) => {
+    const [beer, setBeer] = useState({})
+    const [review, setReview] = useState({})
+    const [loaded, setLoaded] = useState(false)
+    const {slug} = useParams()
+
+    useEffect(() => {
+        const url = `/api/v1/beers/${slug}`
+
+        axios.get(url)
+            .then(resp => {
+                setBeer(resp.data)
+                setLoaded(true)
+            })
+            .catch(resp => console.log(resp))
+    }, [])
+
+    return (
+        <div className="wrapper">
+            <div className="wrapper">
+                {loaded &&
+                    <Header
+                    attributes={beer.data.attributes}
+                    reviews={beer.included}
+                />}
+                <div className="reviews"></div>
+            </div>
+            <div className="wrapper">
+                <div className="review-form">[Review Form]</div>
+            </div>
+        </div>
+    )
 }
 
 export default Beer
