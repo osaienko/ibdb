3e3b0eb1921b8b595fd2f8a93f9c751e999a1525
Oleh Saienko <saenko.oleg1993@gmail.com>
2022-10-18 13:53:03 +0300

    add styling

--- app/javascript/components/Beer/Beer.js
+++ app/javascript/components/Beer/Beer.js
@@ -1,6 +1,31 @@
 import React, { useState, useEffect } from 'react'
 import {useParams} from 'react-router-dom'
 import axios from 'axios'
+import styled from 'styled-components'
+
+const Wrapper = styled.div`
+    margin-left: auto;
+    margin-right: auto;
+    display: grid;
+    grid-template-columns: repeat(2, 1fr);
+`
+
+const Column = styled.div`
+    background: #fff;
+    height: 100vh;
+    overflow: scroll;
+    
+    &:last-child {
+        background: #000;
+    }
+`
+
+const Main = styled.div`
+    padding-left: 50px; 
+`
+
+const ReviewForm = styled.div``
+const Reviews = styled.div``
 
 import Header from './Header'
 
@@ -22,19 +47,21 @@ const Beer = (props) => {
     }, [])
 
     return (
-        <div className="wrapper">
-            <div className="wrapper">
-                {loaded &&
-                    <Header
-                    attributes={beer.data.attributes}
-                    reviews={beer.included}
-                />}
-                <div className="reviews"></div>
-            </div>
-            <div className="wrapper">
+        <Wrapper>
+            <Column>
+                <Main>
+                    {loaded &&
+                        <Header
+                        attributes={beer.data.attributes}
+                        reviews={beer.included}
+                    />}
+                    <div className="reviews"></div>
+                </Main>
+            </Column>
+            <Column>
                 <div className="review-form">[Review Form]</div>
-            </div>
-        </div>
+            </Column>
+        </Wrapper>
     )
 }
 
--- app/javascript/components/Beer/Header.js
+++ app/javascript/components/Beer/Header.js
@@ -1,15 +1,40 @@
 import React from 'react'
+import styled from 'styled-components'
+
+const Wrapper = styled.div`
+    padding: 50px 100px 50px 0;
+    font-size: 30px;
+    
+    img {
+        height: 60px;
+        width: 60px;
+        border-radius: 100%;
+        border: 1px solid rgba(0,0,0,0.1);
+        margin-bottom: -8px;
+    }
+`
+const TotalReviews = styled.div`
+    font-size: 18px;
+    padding: 10px 0;
+`
+const StarRating = styled.div``
+const TotalOutOf = styled.div`
+    font-size: 18px;
+    font-weight: bold;
+    padding: 10px 0;
+`
 
 const Header = (props) => {
     const {name, image_url, avg_score} = props.attributes
     const totalReviews = props.reviews.length
 
     return (
-        <div className="wrapper">
+        <Wrapper>
             <h1><img src={image_url} alt={name}/>{name}</h1>
-            <div className="totalReviews">{totalReviews} User Reviews</div>
-            <div className="totalOutOf">{avg_score} out of 5</div>
-        </div>
+            <TotalReviews>{totalReviews} User Reviews</TotalReviews>
+            <StarRating>skip for now</StarRating>
+            <TotalOutOf>{avg_score} out of 5</TotalOutOf>
+        </Wrapper>
     )
 }
 
